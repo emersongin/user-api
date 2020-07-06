f48260f5aadb3a835c7009ddb7e78cd96fe503d1
Emerson Andrey <emersonandrey_7@hotmail.com>
2020-07-06 15:05:21 -0300

    criate server

---
+++ src/config/database.js
@@ -0,0 +1,11 @@
+module.exports = {
+    dialect: 'postgres',
+    host: 'localhost',
+    database: 'api_users',
+    username: 'postgres',
+    password: 'abc123',
+    define: {
+        timestamps: true,
+        underscored: true
+    }
+};
---
+++ src/database/migrations/20200706142802-create-users.js
@@ -0,0 +1,28 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('users', { 
+      id: {
+        type: Sequelize.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+        allowNull:  false
+      },
+      created_at: {
+        type: Sequelize.DATE,
+        allowNull: false
+      },
+      updated_at: {
+        type: Sequelize.DATE,
+        allowNull: false
+      }
+    });
+
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('users');
+
+  }
+};
